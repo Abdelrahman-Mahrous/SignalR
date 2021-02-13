c1354b6fee0575d8df59b869abbaf000ad15a3ff
Abd-el-rahman Mahrous <abdelrahman.mahrous@outlook.com>
2021-02-13 15:13:34 +0200

    Initial commit

---
+++ SignalRDemo/Controllers/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemo.Controllers
+{
+    public class ChatHub :Hub
+    {
+   
+
+        public async Task SendMessage (string user , string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public async Task SendTime()
+        {
+            var dt = DateTime.Now;           
+            await Clients.All.SendAsync("DT",dt);
+        }
+
+
+
+    }
+}
---
+++ SignalRDemo/wwwroot/js/chat.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+
+document.getElementById("sendButton").disabled = true;
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = user + ' says ' + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    document.getElementById("messageList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (er) {
+    alert(er);
+    return console.error(er.toString());
+    });
+
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var u = document.getElementById("userInput").value;
+    var m = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", u, m).catch(function (er) {
+        alert(er.toString());
+    });
+    event.preventDefault();
+});
+ 
+
+
+
+
+
+
+
